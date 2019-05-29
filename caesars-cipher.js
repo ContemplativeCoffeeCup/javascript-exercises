1d0bd925fa07cc1387cf06cc65f94ea44f937d32
ContemplativeCoffeeCup <michellechristinejohnson@gmail.com>
2019-05-29 16:21:23 -0700

    converted the string to uppercase, the split into an array

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,27 +1,32 @@
 function rot13(str) { //LBH QVQ VG!
+  // convert to uppercase
+  str = str.toUpperCase();
+  // split the string into an array
+  return str.split('')
+  // loop through the characters for the .length of the string
+  // ,map?
+    // for each alphabetic character, grab the charcode
+
+// expected output: Array [2, 8, 18, 32]
+      // convert the charcode to the corresponding key charcode
+
+      // conert the new charcode to character
+
+      // add character to new string
+      
+    //  else return the original character
+    }
 
   return str;
 }
 
 //Change the inputs below to test
-rot13("SERR PBQR PNZC");
+rot13("SERR PBIR PNZC");
 
-
+// Split str into a character array
+      return str.split('')
 
 //uppercase alphabet = CharCode 65 - 90
 //CharCode Conversion:
 //  65-77 (+13)
 //  78-90 (-13)
-
-///// STEPS?: /////
-// convert to uppercase
-// loop through the characters for the .length of the string
-  // for each alphabetic character, grab the charcode
-    // convert the charcode to the corresponding key charcode
-    // conert the new charcode to character
-    // add character to new string
-  //  else return the original character
-
-///// METHODS: /////
-// .charCodeAt(index)
-// .CharAt(index)
