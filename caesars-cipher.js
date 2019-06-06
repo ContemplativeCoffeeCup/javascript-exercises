445e70816a13baf27d5d61019c18a4e4e09cd338
ContemplativeCoffeeCup <michellechristinejohnson@gmail.com>
2019-06-05 20:43:21 -0700

    Created a solution for the caesar cypher

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,36 +1,17 @@
-function rot13(str) { //LBH QVQ VG!
-  // convert to uppercase
-  str = str.toUpperCase();
-  // split the string into an array
-  return str.split('')
-  // loop through the characters for the .length of the string
-for (var i = 0; i < str.length; i++) {
-
-}
-    // for each alphabetic character, grab the charcode
-
-// expected output: Array [2, 8, 18, 32]
-      // convert the charcode to the corresponding key charcode
-
-      // conert the new charcode to character
-
-      // add character to new string
-
-    //  else return the original character
-    }
-
+function rot13(str) { // LBH QVQ VG!
+  var CharArray = [];
+  var regEx = /[A-Z]/ ;
+  str = str.split("");
+  for (var x in str) {
+   if (regEx.test(str[x])) {
+     CharArray.push((str[x].charCodeAt() - 65 + 13) % 26 + 65);
+   } else {
+     CharArray.push(str[x].charCodeAt());
+   }
+  }
+  str = String.fromCharCode.apply(String, CharArray);
   return str;
 }
 
-//Change the inputs below to test
+   // Change the inputs below to test
 rot13("SERR PBIR PNZC");
-
-
-//uppercase alphabet = CharCode 65 - 90
-//CharCode Conversion:
-//  65-77 (+13)
-//  78-90 (-13)
-
-
-    // Change the inputs below to test
-    rot13("LBH QVQ VG!");
